5033be1a16dfa04e902a59b4ea7735710864d204
beginnerbb <dlapdlffldpald1@naver.com>
2021-03-15 18:12:34 +0000

    createElement list

--- todo.js
+++ todo.js
@@ -3,18 +3,32 @@ const toDoForm = document.querySelector(".js-toDoForm"), toDoInput=toDoForm.quer
 const TODOS_LS="toDos";
 
 function paintToDo(text){
-console.log(text);
+const li=document.createElement("li");
+const delBtn=document.createElement("button");
+delBtn.innerText="x";
+const span=document.createElement("span");
+span.innerText=text;
+li.appendChild(span);
+li.appendChild(delBtn);
+toDoList.appendChild(li);
 }
 
 function handleSubmit(event){
 event.preventDefault();
 const currentValue=toDoInput.value;
 paintToDo(currentValue);
+toDoInput.value="";
 }
 
+function loadToDos(){
+const toDos=localStorage.getItem(TODOS_LS);
+if(toDos !==null){
+
+}
+}
 
 function init(){
-
+loadToDos();
 toDoForm.addEventListener("submit",handleSubmit);
 }
 
